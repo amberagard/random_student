414069625c0ce3348ba76a100aa9ab374b5378a8
Amber Agard <amber.n.agard@gmail.com>
2014-09-02 13:39:54 -0500

    installed simple form

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,7 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :students, only: [:index, :new]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
